4c4b32eb8b66a61e1c997b1a39ad522b5f0dd2cc
Richard Ribier <richard.ribier@europe.adp.com>
2011-04-26 22:14:42 +0200

    Added the log
    easy to do in tomcat : getServletContext().log()

--- src/com/ric/test/HelloServlet.java
+++ src/com/ric/test/HelloServlet.java
@@ -11,6 +11,8 @@ public class HelloServlet extends HttpServlet {
     throws ServletException, IOException
   {
     PrintWriter out = res.getWriter();
+    //try the log
+    getServletContext().log("Search this in logs TOTOPOPO");
 
     out.println("Hello, world!");
     out.close();
